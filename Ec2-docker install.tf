11174ab9507dfd1e13eff54b366c87b002b7fe8c
akash <128682166+akashhosamani01@users.noreply.github.com>
2023-12-17 19:59:35 +0530

    Add files via upload

---
+++ Ec2-docker install.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "docker-compose" {
+  ami           = "ami-0461b1436c9b3c1a3"
+  instance_type = "t2.micro"
+
+
+ 
+}
---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAZDXYG6QKW5MTYB6S"
+  secret_key = "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"
+
+}
+
+
+
